3c50cba771390601aac39ed2c5ea2e546f84c846
Tim Pople <timothypople@yahoo.co.uk>
2023-11-27 21:37:59 +0000

    Added a header page

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,39 @@
+import styles from "./styles/Heading.module.css"
+import { useHover } from "./hooks/useHover"
+import { useRef } from "react"
+import TAnimation from "./three/TAnimation";
+import LinkedIn from "./icons/LinkedIn";
+import Github from "./icons/Github";
+import Email from "./icons/Email";
+
 
 const Heading = () => {
+  const ref = useRef(null)
+  const isHeadingHovered = useHover(ref); 
+
   return (
-    <div>
-      <h1>Timothy Pople</h1>
-      <p>Software Engineer, Full Stack & Games Developer</p>
+    <div className={styles.headingContainer}>
+    <TAnimation moveToEnd={isHeadingHovered}/>
+    <div className={styles.textContainer}>
+      <div ref={ref}>
+        <h1>Timothy Pople</h1>
+        <p className={styles.subText}>Software Engineer, Full Stack & Games Developer</p>
+        <div className={styles.iconContainer}>
+            <a href="https://www.linkedin.com/in/timothy-p-54004a9a/?originalSubdomain=uk" className={styles.icon}>
+              <LinkedIn/>
+            </a>
+            <a href="mailto:timothypople@yahoo.co.uk" className={styles.icon}>
+              <Email/>
+            </a>
+            <a href="https://github.com/tpopl01" className={styles.icon}>
+              <Github/>
+            </a>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
+
 
 export default Heading
