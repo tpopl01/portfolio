c36c8b6c996d63238cb4a3514fd435b7691e7175
Tim Pople <timothypople@yahoo.co.uk>
2023-11-27 21:41:36 +0000

    Added convienient custom hooks

---
+++ src/components/hooks/useHover.tsx
@@ -0,0 +1,34 @@
+import { RefObject, useEffect, useState } from 'react'
+
+export function useHover<T extends HTMLElement = HTMLElement>(elementRef: RefObject<T>): boolean {
+  const [value, setValue] = useState<boolean>(false)
+
+  const handleMouseEnter = () => setValue(true)
+  const handleMouseLeave = () => setValue(false)
+
+    
+    useEffect(()=>{
+        console.log(elementRef.current)
+        if(elementRef.current){
+            elementRef.current.addEventListener('mouseenter', function() {
+                handleMouseEnter()
+            });
+            elementRef.current.addEventListener('mouseleave', function() {
+                handleMouseLeave()
+            });
+        }
+        
+        return () => {
+            if(elementRef&&elementRef.current){
+                elementRef.current.removeEventListener('mouseenter', function() {
+                    handleMouseEnter()
+                });
+                elementRef.current.removeEventListener('mouseleave', function() {
+                    handleMouseLeave()
+                });
+            }
+        }
+    },[]);
+
+  return value
+}
