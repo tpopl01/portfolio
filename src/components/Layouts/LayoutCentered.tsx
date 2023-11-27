8320a617d61eaca23d3afb27f52bd72dcdbd9bd5
Tim Pople <timothypople@yahoo.co.uk>
2023-11-27 21:44:26 +0000

    Added central styling component

--- src/components/Layouts/LayoutCentered.tsx
+++ src/components/Layouts/LayoutCentered.tsx
@@ -5,7 +5,7 @@ interface IProps{
 
 const LayoutCentered = (props:IProps) => {
   return (
-    <div style={{width:"100%", height:"100vh", display:"flex", flexDirection:"column", gap:"2rem", justifyContent:"center", alignItems:"center"}}>
+    <div style={{width:"100%", minHeight:"100vh", display:"flex", flexDirection:"column", gap:"2rem", justifyContent:"center", alignItems:"center"}}>
         {props.children}
     </div>
   )
